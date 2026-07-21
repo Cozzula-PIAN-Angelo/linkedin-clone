94848cc0381e5167a15468ba83934b6ad8fd4cca
TheRedKardinal <claudio_giasi@icloud.com>
2026-07-21 17:54:57 +0200

    Componente RoleForm fatto e aggiunto a ProfilePage

--- src/features/profile/ProfilePage.tsx
+++ src/features/profile/ProfilePage.tsx
@@ -1,34 +1,57 @@
+import { useState } from "react";
 import type { User } from "../../types";
 import { mockUsers } from "../../mockData";
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import RoleForm from "./RoleForm";
+import type { ExperienceFormData } from "./RoleForm";
 
 function ProfilePage() {
   const user: User = mockUsers[0];
+  const [experiences, setExperencies] = useState<
+    (ExperienceFormData & { id: string })[]
+  >([]);
   return (
-    <Container className="mt-3">
-      <Row className="justify-content-center">
-        <Col xs={12} md={10} lg={6}>
-          <Card className="profile-card">
-            <Card.Body className="d-flex flex-column align-items-center">
-              <img
-                src={user.avatar}
-                alt={`${user.name} ${user.surname}`}
-                className="avatar-profile rounded-circle mb-3"
-              />
-              <Card.Title>
-                {user.name} {user.surname}
-              </Card.Title>
-              <Card.Subtitle className="text-secondary mb-2">
-                {user.job.map((job) => job.title).join("| ")}
-              </Card.Subtitle>
-              <Button className="bg-white text-primary ">
-                Modifica profilo
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container className="mt-3">
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} xl={8} className="px-0 px-md-3">
+            <Card className="profile-card border-0">
+              <Card.Body className="d-flex flex-column align-items-center">
+                <img
+                  src={user.avatar}
+                  alt={`${user.name} ${user.surname}`}
+                  className="avatar-profile rounded-circle mb-3"
+                />
+                <Card.Title>
+                  {user.name} {user.surname}
+                </Card.Title>
+                <Card.Subtitle className="text-secondary mb-2">
+                  {user.job.map((job) => job.title).join("| ")}
+                </Card.Subtitle>
+                <Button className="bg-white text-primary ">
+                  Modifica profilo
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="mt-3">
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} xl={8} className="px-0 px-md-3">
+            <RoleForm
+              onAdd={(exp) =>
+                setExperencies([
+                  ...experiences,
+                  { id: crypto.randomUUID(), ...exp },
+                ])
+              }
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
--- src/features/profile/RoleForm.tsx
+++ src/features/profile/RoleForm.tsx
@@ -1,7 +1,84 @@
-import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import type { SubmitEvent } from "react";
+import { Card, Form, Button, Row, Col } from "react-bootstrap";
 
-function RoleForm() {
-return (
-
-)
+export interface ExperienceFormData {
+  role: string;
+  company: string;
+  period: string;
+  description: string;
 }
+
+interface RoleFormProps {
+  onAdd: (experience: ExperienceFormData) => void;
+}
+
+function RoleForm({ onAdd }: RoleFormProps) {
+  const [form, setForm] = useState<ExperienceFormData>({
+    role: "",
+    company: "",
+    period: "",
+    description: "",
+  });
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    onAdd(form);
+    setForm({ role: "", company: "", period: "", description: "" });
+  };
+
+  return (
+    <Card className="border-0">
+      <Card.Title className="mt-3 ps-3">Aggiungi esperienza</Card.Title>
+      <Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Row className="g-2">
+              <Col xs={12} md={6}>
+                <Form.Control
+                  type="text"
+                  placeholder="Ruolo"
+                  value={form.role}
+                  onChange={(e) => setForm({ ...form, role: e.target.value })}
+                />
+              </Col>
+              <Col xs={12} md={6}>
+                <Form.Control
+                  type="text"
+                  placeholder="Azienda"
+                  value={form.company}
+                  onChange={(e) =>
+                    setForm({ ...form, company: e.target.value })
+                  }
+                />
+              </Col>
+              <Col xs={12} md={12}>
+                <Form.Control
+                  type="text"
+                  placeholder="Periodo (es. 2024 - presente"
+                  value={form.period}
+                  onChange={(e) => setForm({ ...form, period: e.target.value })}
+                />
+              </Col>
+              <Col xs={12} md={12}>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Descrizione (facoltstiva)"
+                  value={form.description}
+                  onChange={(e) =>
+                    setForm({ ...form, description: e.target.value })
+                  }
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+          <Button type="submit" className="d-block ms-auto mt-3">
+            Aggiungi
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default RoleForm;
