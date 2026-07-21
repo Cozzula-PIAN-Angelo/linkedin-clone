fd2212758c5c0f4bf772436c6cc9db6c3322b7f1
Cozzula-PIAN-Angelo <cozzulaangelo@gmail.com>
2026-07-21 16:26:22 +0200

    completata la parte delle news, neccessaria implementazione alla home

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { newsApi } from "../features/news/newsApi";
 
 export const store = configureStore({
   reducer: {
+    [newsApi.reducerPath]: newsApi.reducer,
     // ogni feature aggiunge la propria riga qui, es:
     // auth: authReducer,
     // posts: postsReducer,
     // profile: profileReducer,
     // home: homeReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(newsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/features/news/types.ts
@@ -0,0 +1,6 @@
+export interface NewsItem {
+  id: string;
+  title: string;
+  link: string;
+  meta: string;
+}
