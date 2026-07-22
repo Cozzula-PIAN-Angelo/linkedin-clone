4b234d1af8e2b306c2344e622d442cbd5481556d
Javier21122000 <torresprivato@gmail.com>
2026-07-22 11:56:06 +0200

    main tsx committato

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
 import { store } from "./app/store.ts";
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
